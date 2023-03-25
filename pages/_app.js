dc4b7b51fd8dec572c1de9a3eb7040bd5e9c75eb
Dhananjay Gandhi <dhananjay.gandhi@landmarkgroup.in>
2023-03-25 15:52:35 +0530

    Updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "@mui/material";
+import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from "../styles/theme";
 import Layout from '../components/layout';
 import '../styles/globals.css';
@@ -6,6 +7,7 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Layout>
         <Component {...pageProps} />
       </Layout>
