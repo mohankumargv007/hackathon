fe5c034c1238e807b1feaf5effbabda1dba1b9c2
PrasadEkbote <prasad.ekbote@landmarkwebteam.com>
2023-03-27 16:58:15 +0530

    barcode generation is implemented

--- pages/fixture/barcode/[fid].js
+++ pages/fixture/barcode/[fid].js
@@ -4,8 +4,13 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { supabaseConnection } from '../../../utils/supabase';
 import styles from '../../../styles/Layout.module.css';
+import Barcode from 'react-barcode';
+import { useEffect,useState } from 'react';
+
 
 export async function getServerSideProps(context) {
+
+
   const { fid } = context.query;
 
   // Fetch data from external API
@@ -21,15 +26,19 @@ export async function getServerSideProps(context) {
 }
 
 export default function Fixture(props) {
-  const fixture = _get(props, "data.0", {});
+  const data = props.data[0]
+ const barCode =  `${data.key}&${data.concept_code}&${data.id}`
+  const [Code, setCode] = useState(barCode);
+
+    const fixture = _get(props, "data.0", {});
   return (
     <div className={styles.container}>
       <Stack spacing={2}>
         <p>4 way new format stand 125CM</p>
-        {`Bar code
-        |||||||||||||||||||||||||
-        |||||||||||||||||||||||||`}
-        <Button variant="contained">Print</Button>
+        
+            <Barcode value={Code} />;
+            
+        <Button onClick={window.print} variant="contained">Print</Button>
         <Link href={`/fixture/${fixture.id}`} passHref legacyBehavior><Button variant="contained">Back</Button></Link>
       </Stack>
     </div>
