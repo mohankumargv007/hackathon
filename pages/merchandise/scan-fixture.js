7f83b35f558efeb3f460e640874914a0754b71a4
PrasadEkbote <prasad.ekbote@landmarkwebteam.com>
2023-03-28 12:46:21 +0530

    scanner configured

--- pages/merchandise/scan-fixture.js
+++ pages/merchandise/scan-fixture.js
@@ -1,15 +1,46 @@
 import Link from 'next/link';
 import _get from 'lodash/get';
 import Box from '@mui/material/Box';
+import {TextareaAutosize, Paper} from '@mui/material'
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import { useState } from 'react';
+import Scanner from '../../utils/scanner'
+
 
 export default function Fixture(props) {
+  const [scanner,setScanner] = useState(false)
+const [results,setResults] = useState([])
+
+const handleScanner = ( ) =>{
+    setScanner(true)
+}
+
+// const _scan = () => {
+//   this.setState({ scanning: !this.state.scanning })
+// }
+
+const _onDetected = result => {
+  setResults( [] )
+  setResults([...results].concat([result]))
+}
+
   return (
     <Box paddingX="20px" paddingY="40px">
       <Stack spacing={4}>
         <Link href={`/merchandise/search`} passHref legacyBehavior><Button variant="contained">Arms, Prongs, Shelves</Button></Link>
-        <Link href={`/merchandise/review`} passHref legacyBehavior><Button variant="contained">Scan Fixture</Button></Link>
+        <Button onClick = {handleScanner} variant="contained" >Scan Fixture</Button>
+
+      {scanner ? ( <Paper variant="outlined" style={{marginTop:30, width:640, height:320}}>
+      <Scanner onDetected={_onDetected}/>
+        </Paper>): null}
+
+        <TextareaAutosize
+            style={{fontSize:32, width:320, height:100, marginTop:30}}
+            rowsMax={4}
+            defaultValue={'No data scanned'}
+            value={results[0] ? results[0].codeResult.code : 'No data scanned'}
+        />
       </Stack>
     </Box>
   )
