8ac173888e34ec906a176d98af4fa829134119e0
Rameshbabu T B <rameshbabu.tb@landmarkgroup.in>
2023-03-30 15:26:04 +0530

    Chart type changes

--- pages/reports/index.js
+++ pages/reports/index.js
@@ -1,37 +1,37 @@
 import _get from 'lodash/get';
 import Box from '@mui/material/Box';
 import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
+  BarChart, 
+  Bar, 
+  XAxis, 
+  YAxis, 
+  CartesianGrid, 
+  Tooltip, 
   Legend
 } from "recharts";
 
 export default function Fixture(props) {
   return (
     <Box paddingX="20px" paddingY="40px">
-    <BarChart
-      width={300}
-      height={250}
-      data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="pv" fill="#8884d8" />
-      <Bar dataKey="uv" fill="#82ca9d" />
-    </BarChart>
+      <BarChart
+        width={300}
+        height={200}
+        data={data}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+        <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+      </BarChart>
     </Box>
   )
 }
