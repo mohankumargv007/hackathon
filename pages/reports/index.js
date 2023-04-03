e52a8fb53f0b56433ef9b1c06204ed1e4123c41a
Rameshbabu T B <rameshbabu.tb@landmarkgroup.in>
2023-04-03 11:27:13 +0530

    Dynamic report is added

--- pages/reports/index.js
+++ pages/reports/index.js
@@ -1,4 +1,5 @@
 import _get from 'lodash/get';
+import { supabaseConnection } from '../../utils/supabase';
 import Box from '@mui/material/Box';
 import {
   BarChart, 
@@ -16,7 +17,7 @@ export default function Fixture(props) {
       <BarChart
         width={500}
         height={500}
-        data={data}
+        data={props.data}
         margin={{
           top: 20,
           right: 30,
@@ -25,95 +26,43 @@ export default function Fixture(props) {
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="Group" />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar dataKey="Arms&Hooks&Bars" stackId="a" fill="#8884d8" />
-        <Bar dataKey="Rails&Stands" stackId="a" fill="#82ca9d" />
-        <Bar dataKey="Shelves&Bins" stackId="a" fill="#82fa9d" />
-        <Bar dataKey="Tables" stackId="a" fill="#82fa9d" />
+        
+        <Bar dataKey="Prong" stackId="a" fill="#8884d8" />
+        <Bar dataKey="Arm" stackId="a" fill="#82ca9d" />
+        <Bar dataKey="Shelves&Bins" stackId="a" fill="#3366ff" />
+        <Bar dataKey="Tables" stackId="a" fill="#ff00ff" />
       </BarChart>
     </Box>
   )
 }
 
-const data = [
-  {
-    "Group": "Footwear",
-    "Arms&Hooks&Bars": 2295,
-    "Rails&Stands": "",
-    "Shelves&Bins": 58,
-    "Tables": ""
-  },
-  {
-    "Group": "Accessories",
-    "Arms&Hooks&Bars": 1409,
-    "Rails&Stands": "",
-    "Shelves&Bins": 22,
-    "Tables": ""
-  },
-  {
-    "Group": "Lingerie & Sleepwear",
-    "Arms&Hooks&Bars": 728,
-    "Rails&Stands": 30,
-    "Shelves&Bins": "",
-    "Tables": 5
-  },
-  {
-    "Group": "Children",
-    "Arms&Hooks&Bars": 497,
-    "Rails&Stands": 85,
-    "Shelves&Bins": 6,
-    "Tables": 10
-  },
-  {
-    "Group": "Men",
-    "Arms&Hooks&Bars": 248,
-    "Rails&Stands": 99,
-    "Shelves&Bins": 55,
-    "Tables": 9
-  },
-  {
-    "Group": "Toys & Luggage",
-    "Arms&Hooks&Bars": 294,
-    "Rails&Stands": "",
-    "Shelves&Bins": 55,
-    "Tables": ""
-  },
-  {
-    "Group": "Women",
-    "Arms&Hooks&Bars": 200,
-    "Rails&Stands": 120,
-    "Shelves&Bins": 15,
-    "Tables": 11
-  },
-  {
-    "Group": "Home",
-    "Arms&Hooks&Bars": 28,
-    "Rails&Stands": "",
-    "Shelves&Bins": 111,
-    "Tables": 3
-  },
-  {
-    "Group": "Basics Non Apparels",
-    "Arms&Hooks&Bars": "",
-    "Rails&Stands": 6,
-    "Shelves&Bins": "",
-    "Tables": ""
-  },
-  {
-    "Group": "Toys Boys & Girls",
-    "Arms&Hooks&Bars": "",
-    "Rails&Stands": 2,
-    "Shelves&Bins": "",
-    "Tables": ""
-  },
-  {
-    "Group": "SM-Accessories",
-    "Arms&Hooks&Bars": "",
-    "Rails&Stands": 1,
-    "Shelves&Bins": "",
-    "Tables": ""
-  }
-];
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const supabase = supabaseConnection();
+    let { data, error } = await supabase
+      .from('fixture_product_list')
+      .select('group,department, linear_meter,fixture_library:fixture_key (type)')
+      .eq('store_id', 60318);
+    let finalResult = {}
+    let spaceTypes = {}
+    data.forEach(function (item, index) {
+      spaceTypes[item.fixture_library.type] = item.fixture_library.type
+      if (finalResult.hasOwnProperty(item.group)) {
+        if (finalResult[item.group].hasOwnProperty(item.group)) {
+          finalResult[item.group][item.fixture_library.type] = finalResult[item.group][item.fixture_library.type] + item.linear_meter
+        } else {
+          finalResult[item.group][item.fixture_library.type] =  item.linear_meter;
+        }
+      } else {
+        finalResult[item.group] = {
+          "Group" : item.group
+        }
+        finalResult[item.group][item.fixture_library.type] =  item.linear_meter;
+      }
+    });
+    return { props: { data: Object.values(finalResult), "key_values" : Object.keys(spaceTypes)} };
+}
