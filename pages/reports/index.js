41aac8580bc1007bf3ee39c92b5f4bca08e13756
Rameshbabu T B <rameshbabu.tb@landmarkgroup.in>
2023-04-04 17:00:05 +0530

    Added function in DB to do the grouping operation

--- pages/reports/index.js
+++ pages/reports/index.js
@@ -50,15 +50,11 @@ export async function getServerSideProps() {
   // Fetch data from external API
   const supabase = supabaseConnection();
     let { data, error } = await supabase
-      .from('fixture_product_list')
-      .select('group,department, linear_meter,fixture_library:fixture_key (type)')
-      .eq('store_id', 60318);
+      .rpc('get_report_data', {'store_id' : 60318})
     let finalResult = {}
-    let spaceTypes = {}
     data.forEach(function (item, index) {
-      spaceTypes[item.fixture_library.type] = item.fixture_library.type
-      finalResult[item.group] = finalResult[item.group] ?? {"Group" : item.group}
-      finalResult[item.group][item.fixture_library.type] =  finalResult[item.group][item.fixture_library.type] ? (finalResult[item.group][item.fixture_library.type] + item.linear_meter) : item.linear_meter
+      finalResult[item.gname] = finalResult[item.gname] ?? {"Group" : item.gname}
+      finalResult[item.gname][item.gtype] =  finalResult[item.gname][item.gtype] ? (finalResult[item.gname][item.gtype] + item.lm) : item.lm
     });
-    return { props: { data: Object.values(finalResult), "key_values" : Object.keys(spaceTypes)} };
+    return { props: { data: Object.values(finalResult)} };
 }
