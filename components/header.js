7d18231d254ad97ceb21afd0a99fe8e7b33ca41d
Dhananjay Gandhi <dhananjay.gandhi@landmarkgroup.in>
2023-04-06 00:21:24 +0530

    Minor: code refactoring. - Removed commented and unused code.

--- components/header.js
+++ components/header.js
@@ -3,9 +3,8 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Link from 'next/link';
-import { useAppContext } from '../contexts/appContext';
+
 export default function Header(props) {
-  // const { title } = useAppContext();
   const { title } = props;
   return (
     <Box sx={{ flexGrow: 1 }}>
