6883296bc6be1c0016bc35bbcb3a6b648f74e4c1
Dhananjay Gandhi <dhananjay.gandhi@landmarkgroup.in>
2023-04-06 00:12:33 +0530

    feature: scandit integration in merchandise/scan-fixture page.

---
+++ components/scandit.js
@@ -0,0 +1,143 @@
+import { useRef, useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+import * as SDCCore from "scandit-web-datacapture-core";
+import * as SDCBarcode from "scandit-web-datacapture-barcode";
+
+const libraryLocation = 'https://cdn.jsdelivr.net/npm/scandit-web-datacapture-barcode@6.x/build/engine/';
+const licenseKey = 'AfZydWVhRvlIKSgyqzoAZw0OHeoICxgiFXidHWkiBNKGR70WBHcavoB+A/Fhde//+lhijfd17iJAcD0+31VRTBpb2olPbbidp0me/Y0x93ZdKL0BpiC2WXoBWs2jGMNp242GhUh4UiC2DGSwtxJP8AbWXRM90PDWQM+Ylfi+sg+GFHTiSzRHj5sl/PbYJ/PS+G3eOWsBgY2IwjbMZF8nMkiHrTSN5as5Ro4MvkzFh4kQmcpTygh2Q9evtzdxQ2HVdrtDLyEghW2SjLTsrLU8hf524d+Mr3MgUNPDg3kJxQpXjLO2IdMUaxfmd5KmYM/ndBj7PulfiJMpKIqzqTQWy9FyQZmx0mipncKDUXe7eg8azirRRleZPa9w08DV4f2TWQd1Uk1SWeVO9akXbtKBN7OEu8BbPwvqhqXWRLW2xYcckHQ2mdX1LQ9qgHe/qR49k2GrezxLQ9F8wlwIRidr2RbV0NKd8BEIxh9Rk3UqjaAVXlXUGMyQRf21KxXaDcCC0XQL3QVRe1ZCCIcCWSegYw2tf2GGJvf/sk0tLjel86G0huIuNW0hT2irJY0sW2II+66M8NHkjI9p32w4oL9QgD8pkWlOZP+ySXYYrLnO2gEEhh73WY4s1ij/fSAxB2mEXJiX8iDpv7BR0eKneZ3pqdrP8DT2Yw7pEZ6/Qd/IdoNiOfWZnuyD2GW1fw3UtsqqNZx0om5Xl0d4b7LygwBujsuXbvcBLaz8ZuOv7jbAO5duzsl/hYsbRqQSh8FFgU6+CLOEXz38j1oJQbcEoqNvLxV6AoJ09syliWDRHrDwBA==';
+
+export default function Scandit(props) {
+  const { btnText } = props;
+  const [value, setValue] = useState("");
+  // Keep a reference to the context object.
+  let context = useRef();
+  // Keep a reference to the barcode capture mode object.
+  let barcodeCapture = useRef();
+
+  async function loadAndPrepareLibrary() {
+    // Configure and load the library using your license key. The passed parameter represents the location of the wasm
+    // file, which will be fetched asynchronously. You must `await` the returned promise to be able to continue.
+    await SDCCore.configure({
+      licenseKey: licenseKey,
+      libraryLocation: libraryLocation,
+      moduleLoaders: [
+        SDCBarcode.barcodeCaptureLoader({ highEndBlurryRecognition: false })
+      ]
+    })
+
+    // Create the data capture context.
+    context = await SDCCore.DataCaptureContext.create()
+
+    // Try to use the world-facing (back) camera and set it as the frame source of the context. The camera is off by
+    // default and must be turned on to start streaming frames to the data capture context for recognition.
+    await context.setFrameSource(SDCCore.Camera.default)
+
+    // The barcode capturing process is configured through barcode capture settings,
+    // they are then applied to the barcode capture instance that manages barcode recognition.
+    const settings = new SDCBarcode.BarcodeCaptureSettings()
+
+    // The settings instance initially has all types of barcodes (symbologies) disabled. For the purpose of this
+    // sample we enable a very generous set of symbologies. In your own app ensure that you only enable the
+    // symbologies that your app requires as every additional enabled symbology has an impact on processing times.
+    settings.enableSymbologies([
+      SDCBarcode.Symbology.EAN13UPCA,
+      SDCBarcode.Symbology.EAN8,
+      SDCBarcode.Symbology.UPCE,
+      SDCBarcode.Symbology.QR,
+      SDCBarcode.Symbology.DataMatrix,
+      SDCBarcode.Symbology.Code39,
+      SDCBarcode.Symbology.Code128,
+      SDCBarcode.Symbology.InterleavedTwoOfFive
+    ])
+
+    // Create a new barcode capture mode with the settings from above.
+    barcodeCapture = await SDCBarcode.BarcodeCapture.forContext(
+      context,
+      settings
+    )
+    // Disable the barcode capture mode until the camera is accessed.
+    await barcodeCapture.setEnabled(false)
+
+    // Register a listener to get informed whenever a new barcode got recognized.
+    barcodeCapture.addListener({ didScan })
+
+    // To visualize the ongoing barcode capturing process on screen, set up a data capture view that renders the
+    // camera preview. The view must be connected to the data capture context.
+    const view = await SDCCore.DataCaptureView.forContext(context)
+
+    // Connect the data capture view to the HTML element.
+    view.connectToElement(document.getElementById('data-capture-view'))
+
+    // Add a control to be able to switch cameras.
+    view.addControl(new SDCCore.CameraSwitchControl())
+  }
+
+  // Close the modal and switch off the camera.
+  async function closeScanner() {
+    await wait(300)
+    await context.frameSource.switchToDesiredState(SDCCore.FrameSourceState.Off)
+    const dataCaptureView = document.getElementById("data-capture-view");
+    if(dataCaptureView) dataCaptureView.style.height = "0px";
+  }
+
+  // Open our modal and start the camera to scan a barcode.
+  async function openScanner() {
+    document.getElementById("data-capture-view").style.height = "auto";
+    await wait(300)
+    // Start the camera. This can potentially fail, so we use try/catch.
+    try {
+      await context.frameSource.switchToDesiredState(
+        SDCCore.FrameSourceState.On
+      )
+      await barcodeCapture.setEnabled(true)
+    } catch (error) {
+      const reason =
+        typeof error === "object" &&
+        error != null &&
+        typeof error["toString"] === "function"
+          ? error.toString()
+          : "unknown error"
+      alert(`Could not start camera: ${reason}`)
+      await closeScanner()
+    }
+  }
+
+  // When a scan happened, we populate the input and close the modal.
+  async function didScan(barcodeCaptureMode, session) {
+    await barcodeCapture.setEnabled(false)
+    const barcode = session.newlyRecognizedBarcodes[0]
+    await closeScanner();
+    setValue(barcode.data ?? "");
+  }
+
+  // Wait for X milliseconds
+  async function wait(ms) {
+    return new Promise(resolve => {
+      setTimeout(resolve, ms)
+    })
+  }
+
+  useEffect(() => {
+    // Load the library as soon as possible. This will make the user experience faster.
+    loadAndPrepareLibrary();
+    return () => {
+      closeScanner();
+    };
+  }, []);
+
+  useEffect(() => {
+    // This will return the scanned value to parent component.
+    value.length > 1 && props.onDetected(value);
+  }, [value]);
+  
+  const handleScanner = () => {
+    openScanner();
+  }
+
+  return (
+    <>
+      <Button onClick={handleScanner} variant="contained" >{btnText}</Button>
+      <div id="data-capture-view"></div>
+    </>
+  )
+}
--- pages/merchandise/scan-fixture.js
+++ pages/merchandise/scan-fixture.js
@@ -1,44 +1,39 @@
 import { useState } from 'react';
+import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import _get from 'lodash/get';
 import Box from '@mui/material/Box';
-import { Paper, TextField, Alert } from '@mui/material';
-import { verify } from 'crypto';
-import Quagga from 'quagga';
+import { TextField, Alert } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Layout from '../../components/layout';
-import Scanner from '../../utils/scanner';
+
+const Scandit = dynamic(() => import('../../components/scandit'), {
+  ssr: false,
+})
 
 export default function Fixture(props) {
   const router = useRouter();
-  const [scanner, setScanner] = useState(false)
   const [results, setResults] = useState([])
   const [error, setError] = useState(false)
 
-  const handleScanner = () => {
-    setScanner(true)
-  }
-
   const handleProceed = async () => {
-    const code = _get(results, "0.codeResult.code");
-    const url = `/api/fixture/barcode/${results[0].codeResult.code}`;
+    const code = _get(results, "0");
+    const url = `/api/fixture/barcode/${code}`;
     const response = await fetch(url);
     const { data, error } = await response.json();
     if (data.length) {
-      const code = _get(results, "0.codeResult.code");
       router.push(`/merchandise/barcode/${code}`);
     } else {
       setError(true);
       setResults([]);
     }
-
   }
 
   const _onDetected = result => {
-    setResults([])
-    if (result.codeResult.code.length == 13) {
+    setResults([]);
+    if (result.length == 13) {
       setResults([result])
     }
   }
@@ -48,18 +43,14 @@ export default function Fixture(props) {
       <Box paddingX="20px" paddingY="40px">
         <Stack spacing={4}>
           <Link href={`/merchandise/search`} passHref legacyBehavior><Button variant="contained">Arms, Prongs, Shelves</Button></Link>
-          <Button onClick={handleScanner} variant="contained" >Scan Fixture</Button>
-          {scanner ? (<Paper variant="outlined" style={{ marginTop: 30, minWidth: 320, height: 320 }}>
-            <Scanner onDetected={_onDetected} />
-          </Paper>) : null}
-
+          <Scandit btnText="Scan Fixture" onDetected={_onDetected} />
           <TextField
-            style={{ fontSize: 50, width: 320, height: 70, marginTop: 30 }}
+            style={{ fontSize: 50, width: 320, height: 40 }}
             rowsmax={4}
             type='number'
-            value={results[0] ? results[0].codeResult.code : 'No data scanned'}
+            value={results[0] ? results[0] : 'No data scanned'}
             onChange={event => {
-              setResults([{ codeResult: { code: event.target.value } }]);
+              setResults([event.target.value]);
               error && setError(false);
             }}
           />
