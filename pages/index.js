ebf78011c80fe3d463ab291031f8d1e4c9d0adbc
Dhananjay Gandhi <dhananjay.gandhi@landmarkgroup.in>
2023-04-14 12:00:24 +0530

    Feat: fixed unit test cases

--- pages/index.js
+++ pages/index.js
@@ -8,25 +8,23 @@ import styles from '../styles/Home.module.css';
 export default function Home(props) {
   return (
     <Layout title="SMT">
-      <Box paddingX="20px" paddingY="30px">
-        <Stack spacing={2} sx={{ width: "100%" }}>
-          <Link className={styles.btn} href="/fixture/search" passHref legacyBehavior>
-            <Button role="button" variant="contained" className={styles.btn}>Barcode a Fixture</Button>
-          </Link>
-          <Link className={styles.btn} href="/merchandise/scan-fixture" passHref legacyBehavior>
-            <Button variant="contained" className={styles.btn}>Map Merchandise</Button>
-          </Link>
-          <Link className={styles.btn} href="/fixture/remove" passHref legacyBehavior>
-            <Button variant="contained" className={styles.btn}>Remove Fixture</Button>
-          </Link>
-          <Link className={styles.btn} href="/adjacencies" passHref legacyBehavior>
-            <Button variant="contained" className={styles.btn}>Map Adjacencies</Button>
-          </Link>
-          <Link className={styles.btn} href="/reports" passHref legacyBehavior>
-            <Button variant="contained" className={styles.btn}>Reports</Button>
-          </Link>
-        </Stack>
-      </Box>
+      <Stack spacing={2} sx={{ width: "100%" }}>
+        <Link className={styles.btn} href="/merchandise/scan-fixture" passHref legacyBehavior>
+          <Button role="map-merchandise" variant="contained" className={styles.btn} size="large">Map Merchandise</Button>
+        </Link>
+        <Link className={styles.btn} href="/fixture/remove" passHref legacyBehavior>
+          <Button variant="contained" className={styles.btn} size="large">Remove Fixture</Button>
+        </Link>
+        <Link className={styles.btn} href="/fixture/search" passHref legacyBehavior>
+          <Button variant="contained" className={styles.btn} size="large">Barcode a Fixture</Button>
+        </Link>
+        <Link className={styles.btn} href="/adjacencies" passHref legacyBehavior>
+          <Button variant="contained" className={styles.btn} size="large">Map Adjacencies</Button>
+        </Link>
+        <Link className={styles.btn} href="/reports" passHref legacyBehavior>
+          <Button variant="contained" className={styles.btn} size="large">Reports</Button>
+        </Link>
+      </Stack>
     </Layout>
   )
 }
